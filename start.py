e0c7a1279989f993b6de2bf1f3047776ded8b01e
HolubStepan <stepan.holub@partner.magna.com>
2023-07-12 16:09:33 +0200

    add turtle

---
+++ start.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+my_turtle = Turtle()
+my_turtle.shape("turtle")
+my_turtle.color("coral")
+my_turtle.forward(50)
+my_turtle.right(90)
+my_turtle.forward(50)
+
+screen = Screen()
+screen.exitonclick()
